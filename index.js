5ce175cff66d3cfa42d221411c2b1e715399e0ec
Erlend Hamberg <eh@blank.no>
2017-01-16 10:27:19 +0100

    Complete bot

--- index.js
+++ index.js
@@ -5,41 +5,71 @@ const WebClient = require('@slack/client').WebClient;
 const jwt = require('jsonwebtoken');
 const format = require('date-fns/format');
 const subDays = require('date-fns/sub_days');
+const subWeeks = require('date-fns/sub_weeks');
+const nbLocale = require('date-fns/locale/nb');
 
 const slack = new WebClient(process.env.SLACK_API_TOKEN || '');
 
-const startDate = format(subDays(new Date(), 7), 'YYYY-MM-DD');
-const endDate = format(subDays(new Date(), 1), 'YYYY-MM-DD');
+const startDate = subWeeks(new Date(), 1);
+const endDate = subDays(new Date(), 1);
+
+const nbDays = {
+  1: 'én dag',
+  2: 'to dager',
+  3: 'tre dager',
+  4: 'fire dager',
+  5: 'fem dager'
+};
+
+const greetings = [
+  'God dag.',
+  'Insjill.',
+  '¡Buenos diaz!',
+  'Buongiorno.',
+  '¡Hola!',
+  'Hej!',
+  'Selamat pagi!',
+  'Guten tag.',
+  'Tjena!'
+];
 
 async function notifySlakcers() {
+  const apiToken = jwt.sign({role: 'root'}, process.env.API_JWT_SECRET || 'dev-secret-shhh');
+
   const notifiees = await request({
-    uri: 'https://api-blank-test.floq.no/rpc/time_tracking_status',
+    uri: 'https://api-blank.floq.no/rpc/time_tracking_status',
     method: 'POST',
     json: true,
     headers: {
-      authorization: `Bearer ${jwt.sign({role: 'root'}, process.env.API_TOKEN || 'dev-secret-shhh')}`
+      authorization: `Bearer ${apiToken}`
     },
     body: {
-      start_date: startDate,
-      end_date: endDate
+      start_date: format(startDate, 'YYYY-MM-DD'),
+      end_date: format(endDate, 'YYYY-MM-DD')
     }
   })
   .then(employees => employees.filter(({unregistered_days}) => unregistered_days > 0));
 
   const {members: slackUsers} = await slack.users.list();
 
-  for (let {email, unregistered_days} of notifiees) {
+  for (let {email, unregistered_days: days} of notifiees) {
     const targetUser = slackUsers.find(u => u.profile.email === email);
 
     if (targetUser === undefined) {
       console.error(`Slack user for email ${email} not found.`);
     } else {
-      console.log(`Notifying user @${targetUser.name} that s/he is missing ${unregistered_days} day(s).`);
+      const firstDate = format(startDate, 'Do MMMM', {locale: nbLocale});
+      const lastDate = format(endDate, 'Do MMMM', {locale: nbLocale});
+      const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
-      // if (targetUser.name === 'eh') {
-      //   slack.chat.postMessage('@eh', 'Hello there')
-      //     .then(_ => console.log('Message sent: ', res));
-      // }
+      const message = `${greeting} Det ser ut som De har glemt å føre ${nbDays[days]} `
+            + `sist uke (mellom ${firstDate} og ${lastDate}).`;
+
+      console.log(`Notifying user @${targetUser.name} that s/he is missing ${days} day(s).`);
+      console.log(message);
+
+      slack.chat.postMessage(`@${targetUser.name}`, message, {as_user: true})
+        .then(_ => console.log(`Message sent to ${targetUser.name}`));
     }
   }
 }
